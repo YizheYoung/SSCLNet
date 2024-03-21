d660efaf6e21c8c7ec5c0ba94992898a83101092
YizheYoung <164346842+YizheYoung@users.noreply.github.com>
2024-03-21 20:51:38 +0800

    Add files via upload

---
+++ models/model_zoo.py
@@ -0,0 +1,20 @@
+from models.sseg.deeplabv3plus import DeepLabV3Plus
+from models.sseg.fcn import FCN
+from models.sseg.pspnet import PSPNet
+from models.sseg.unet import UNet
+from models.sseg.ocr import OCR
+
+def get_model(model, backbone, pretrained, nclass, lightweight):
+    if model == "fcn":
+        model = FCN(backbone, pretrained, nclass, lightweight)
+    elif model == "pspnet":
+        model = PSPNet(backbone, pretrained, nclass, lightweight)
+    else:
+        exit("\nError: MODEL \'%s\' is not implemented!\n" % model)
+
+    params_num = sum(p.numel() for p in model.parameters())
+    print("\nParams: %.1fM" % (params_num / 1e6))
+
+    return model
+
+
